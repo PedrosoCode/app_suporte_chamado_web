b01ea223567c8be4d3c626c1417f88c9005f7f4b
gabriel <gb.ribeiropedroso@gmail.com>
2025-07-05 23:17:48 -0300

    rascunho infor. card suporte

--- src/views/suporteHub.vue
+++ src/views/suporteHub.vue
@@ -44,31 +44,50 @@ onMounted(() => {
 
       <!-- Lista de suportes -->
       <div class="space-y-6">
-        <div
-          v-for="suportes in dadoChamados"
-          :key="suportes.nCodigoAcesso"
-          class="flex flex-col md:flex-row bg-white rounded-2xl shadow-md overflow-hidden"
-        >
+        <div v-for="suportes in dadoChamados" :key="suportes.nCodigoAcesso"
+          class="flex flex-col md:flex-row bg-white rounded-2xl shadow-md overflow-hidden hover:outline-emerald-400 hover:outline-6 transition-transform duration-200 hover:translate-3 hover:rotate-z-1 hover:skew-x-1 ">
           <!-- Card principal -->
           <div class="md:w-10/12 border-r">
             <div class="border-b px-6 py-4 bg-emerald-100 text-emerald-800 font-semibold">
               {{ suportes.sNomeEmpresa }}
             </div>
-            <div class="px-6 py-4 space-y-2">
+            <div class="px-6 py-2 space-y-2">
               <p class="text-gray-800 font-medium">
                 {{ suportes.sUsuarioAbertura }} - {{ suportes.sAssuntoChamado }} - {{ suportes.nCodigoAcesso }}
               </p>
-              <footer class="text-sm text-gray-500">
-                Breve descrição do que está acontecendo no sistema
+              <footer class="text-sm text-gray-500 space-y-1">
+                <div>
+                  Breve descrição do que está acontecendo no sistema
+                </div>
+                <div>
+                  Data Abertura xx/xx/xxxx - x dias atrás
+                </div>
               </footer>
+            </div>
+            <div class="flex gap-2 p-2">
+              <div
+                class="rounded-md bg-blue-600 py-0.5 px-2.5 border border-transparent text-sm text-white transition-all shadow-sm">
+                Pedroso
+              </div>
+              <div
+                class="rounded-md bg-red-600 py-0.5 px-2.5 border border-transparent text-sm text-white transition-all shadow-sm">
+                Alguém
+              </div>
+              <div
+                class="rounded-md bg-green-600 py-0.5 px-2.5 border border-transparent text-sm text-white transition-all shadow-sm">
+                Usuário
+              </div>
+
+            </div>
+            <div class="bg-red-300 p-2 text-center text-yellow-900">
+              <p>Δ Este suporte ainda não foi atendido</p>
             </div>
           </div>
 
           <!-- Prioridade -->
           <div class="md:w-2/12 flex items-center justify-center bg-gray-50 p-4">
             <div class="text-center">
-              <div class="text-sm text-gray-600 mb-1">N° Prioridade</div>
-              <div class="text-4xl font-bold text-emerald-600">15</div>
+              <div class="text-4xl font-bold text-emerald-600">Baixa</div>
             </div>
           </div>
         </div>
